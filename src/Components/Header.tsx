541ffed9f0204e645f145193a9be0912e5c63f6b
HimilceSanchez <himisan27@gmail.com>
2025-04-25 09:36:37 +0200

    hh

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,10 +1,9 @@
-
 function Header() {
     const menu = [
         {
             name: "INICIO",
             id: 1,
-            href: "#inicio"
+            
         },
         {
             name: "HABILIDADES",
@@ -14,27 +13,23 @@ function Header() {
         {
             name: "CONTACTO",
             id: 3
+            
         },
-       
+    ];
 
-    ]
     return (
-        <div className="flex items-center 
-        w-screen justify-between border-b-[1px] border-purple-100 ">
+        <div className="flex items-center w-full max-w-[100vw] justify-between border-b-[1px] border-purple-100 overflow-x-hidden">
             <div className="w-[90px] h-[90px] bg-purple-100">
-                <img src="/logo.png" alt="logo"
-                className="p-1" />
+                <img src="/logo.png" alt="logo" className="p-1" />
             </div>
             <div className="hidden md:flex gap-16">
                 {menu.map((item) => (
-                    <div className="cursor-pointer
-                hover:underline font-medium">
+                    <div key={item.id} className="cursor-pointer hover:underline font-medium">
                         <h2>{item.name}</h2>
                     </div>
-
                 ))}
             </div>
-            <div className="w-[90px] h-[90px] bg-purple-300 flex justify-center items-center ">
+            <div className="w-[90px] h-[90px] bg-purple-300 flex justify-center items-center">
                 <svg xmlns="http://www.w3.org/2000/svg"
                     fill="none" viewBox="0 0 24 24"
                     strokeWidth={1.5}
@@ -45,12 +40,9 @@ function Header() {
                         strokeLinejoin="round"
                         d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
                 </svg>
-
             </div>
-
         </div>
-    )
+    );
 }
 
-export default Header
-
+export default Header;
