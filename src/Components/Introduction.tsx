4c346ec7dd4e7f58db94dbafab2fa50336572f2e
HimilceSanchez <himisan27@gmail.com>
2025-04-25 12:17:55 +0200

    i

--- src/Components/Introduction.tsx
+++ src/Components/Introduction.tsx
@@ -5,7 +5,7 @@ function Introduction() {
           <div className='w-[5px] h-[5px] bg-purple-300 rounded-full'></div>
           <h2 className='mt-5 text-black font-medium text-[12px]
           tracking-widest '>HOLA! MI NOMBRE ES</h2>
-          <h2 className='text-[70px] font-bold text-black tracking-widest
+          <h2 className='text-[50px] font-bold text-black tracking-widest
           mt-5'>HIMILCE <br></br>SANCHEZ</h2>
           <img src="/avatar.png"
           className='w-[160px] h-[160xp] bg-gray-200 p-7 rounded-full'
