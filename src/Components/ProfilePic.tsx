cc3f91a1ce477f6cab96a2ee4c9585d603529e73
HimilceSanchez <himisan27@gnmail.com>
2025-10-22 23:09:23 +0200

    ioi

--- src/Components/ProfilePic.tsx
+++ src/Components/ProfilePic.tsx
@@ -1,7 +1,7 @@
 function ProfilePic() {
     return (
       <div className='fixed mt-[80px]'>
-          <img src="/soyyo.jpeg"
+          <img src="/soyyo.jpg"
           className='h-screen object-cover w-screen'/>
       </div>
     )
